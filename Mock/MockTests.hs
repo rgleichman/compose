93b573810da59f3c9c28afa38ebbba7a5f8d10b7
Robbie Gleichman <rgleichman@gmail.com>
2015-05-28 20:26:48 -0700

    changes

--- Mock/MockTests.hs
+++ Mock/MockTests.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module MockTests where
 
 import MockWrappers
 import MockTags
 import Apply
+
+--TODO: Investigate mocking mocks (without needing unwrapFunc) while keeping it easy to mock returned functions
 
 --Method and WrapMath
 data Method = Negate | Square | PowerOfTwo
@@ -16,7 +19,7 @@ instance TagC AMet_aa Method (a->a) where
 instance Num a => App AMet_aa WrapMath Method (a -> a) where
   apply = makeApplyInst
           (\f m -> case m of
-                    Negate -> (\x -> (f Square x) - 10)
+                    Negate -> (\x -> (f Square x) * (f Negate x + 1))
                     _ -> f m)
 
 parentObject :: Method -> Integer -> Integer
@@ -30,6 +33,25 @@ testMethod x = showX ++ show (fmap unwrap [neg, sqr, two])
     [neg, sqr, two] = fmap (\method -> wApply2 AMet_aa Aa_a parentObject (wrap method) x) methods
     showX = unwrap $ show #@ ADef & x
     methods = [Negate, Square, PowerOfTwo]
+
+unwrapFunc :: Wrap w => (w a -> w b) -> w (a->b)
+unwrapFunc f = wrap (\x -> unwrap $ f (wrap x))
+
+testDoubleInherit :: (App TAa_a w Integer Integer, App AMet_aa w Method (Integer -> Integer)) => w Integer -> String
+testDoubleInherit x = showX ++ show (fmap unwrap [neg, sqr, two])
+  where
+    [neg, sqr, two] = fmap func methods
+    child1 :: (App AMet_aa w Method (Integer -> Integer)) => w Method -> w (Integer -> Integer)
+    child1 = wApply parentObject AMet_aa
+    child1Unwrapped = unwrapFunc child1
+    --func method = wApply2 AMet_aa Aa_a parentObject (wrap method) x
+    --func method = apply (child1 (wrap method)) Aa_a x
+    --func method = apply (apply child1Unwrapped AMet_aa (wrap method)) Aa_a x
+    --func method = applyW child1Unwrapped AMet_aa method @@ Aa_a  & x
+    func method = child1Unwrapped @# AMet_aa & method @@ Aa_a  & x
+    showX = unwrap $ show #@ ADef & x
+    methods = [Negate, Square, PowerOfTwo]
+
 
 applyTwice :: (App TAa_a w b b, Show b, Num b) => w b -> String
 applyTwice x = unwrap (wApply show ADef x) ++ (show . unwrap $ wApply (+1) Aa_a x)
